b13fd424ce78dd5f1d22778c50f024599029af8b
Sjors van Gelderen <sjors@openmailbox.org>
2017-06-08 10:08:19 +0200

    Adding new iterations

---
+++ Bonus Samples/Factory.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Program
+{
+    /*
+        Factory example
+    */
+    
+    
+    class Program
+    {
+        static void Main()
+        {
+	    
+        }
+    }
+}
